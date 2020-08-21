bf7974353d55d9231b0f3bf7fa09daca3f122590
vuongfly <68699694+vuongfly@users.noreply.github.com>
2020-08-21 17:33:35 +0700

    bai tap js buoi 2

--- techmaster/source-code/javascript/js-array-object-lession2/practive.js
+++ techmaster/source-code/javascript/js-array-object-lession2/practive.js
@@ -13,10 +13,34 @@ function sumBetweenNum(a, b) {
     return sum;
 }
 // 3
-function checkPrimeNumber(number){
-    
+function checkPrimeNumber(number) {
+    if (!Number.isInteger(number) || number < 2) return false;
+    if (number == 2) return true;
+    for (i = 2; i < Math.sqrt(number); i++) {
+        if (number % i == 0) return false;
+    }
+    return true;
 }
 
+// 4
+function sumPrimeNumber(number) {
+    let sum = 0;
+    for (let i = 0; i <= number; i++) {
+        if (checkPrimeNumber(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+// 5
+function sumOfDivisor(number) {
+    let sum = 0;
+    for (i = 2; i <= number; i++) {
+        if (number % i == 0) sum += i;
+    }
+    return sum;
+}
 
 // 6
 function getKey(ob) {
@@ -76,4 +100,29 @@ function sortByAge(params) {
     return params.sort(function (a, b) {
         return (a.age - b.age);
     });
+}
+
+// 12
+
+function symmetricDifference(arr1, arr2) {
+    arr1.sort((a, b) => a - b);
+    arr2.sort((a, b) => a - b);
+    for (i = 0; i < arr1.length; i++) {
+        for (j = 0; j < arr2.length; j++) {
+            if (arr1[i] < arr2[j]) {
+                break;
+            }
+            if (arr1[i] > arr2[j]) {
+                continue;
+            }
+            if (arr1[i] == arr2[j]) {
+                arr1.splice(i--, 1);
+                arr2.splice(j--, 1);
+                break;
+            }
+        }
+    }
+    console.log(arr1);
+    console.log(arr2);
+    return arr1.concat(arr2);
 }
