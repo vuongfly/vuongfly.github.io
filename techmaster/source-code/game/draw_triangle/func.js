b001f5e601c986f225a3a9421339dafdb55057e7
vuongfly <68699694+vuongfly@users.noreply.github.com>
2020-09-11 14:41:11 +0700

    draw triangle

---
+++ techmaster/source-code/game/draw_triangle/func.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = innerWidth * 0.8;
+canvas.height = innerHeight * 0.8;
+canvas.style.border = '1px solid';
+
+function randomNumber(start, end) {
+    return Math.floor(Math.random() * (end - start)) + start;
+}
+
+class Point {
+    constructor(x, y) {
+        this.x = x; this.y = y;
+    }
+}
+
+function createPoint(width, height) {
+    return new Point(randomNumber(0, width), randomNumber(0, height));
+}
+
+let A = createPoint(canvas.width, canvas.height);
+let B = createPoint(canvas.width, canvas.height);
+let C = createPoint(canvas.width, canvas.height);
+
+function drawTriangle(A, B, C) {
+    ctx.beginPath();
+    ctx.moveTo(A.x, A.y);
+    ctx.lineTo(B.x, B.y);
+    ctx.lineTo(C.x, C.y);
+    ctx.lineTo(A.x, A.y);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawMedianLine(A, B, C) {
+    ctx.beginPath();
+    ctx.moveTo(A.x, A.y);
+    ctx.lineTo((B.x + C.x) / 2, (B.y + C.y) / 2);
+    ctx.strokeStyle = 'blue';
+    ctx.setLineDash([5, 10]);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+    drawTriangle(A, B, C)
+    drawMedianLine(A, B, C)
+    drawMedianLine(B, A, C)
+    drawMedianLine(C, A, B)
+}
+
+animate();
